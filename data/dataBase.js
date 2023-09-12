4be6e3fca9d27fac3f8696d374f1179bb76dcadf
Aniket Matkar <aniketmatkar628@gmail.com>
2023-09-12 13:50:14 +0530

    configuring cors for deployment

--- data/dataBase.js
+++ data/dataBase.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017", {
+    .connect(process.env.MONGO_URI, {
       dbName:"toDoBackend",
       useNewUrlParser: true,
       useUnifiedTopology: true,

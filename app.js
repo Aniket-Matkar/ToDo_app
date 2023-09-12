63bdd5f5bcfb325aa41a479c87bb63d543181868
Aniket Matkar <aniketmatkar628@gmail.com>
2023-09-12 13:34:26 +0530

    configuring cors for deployment

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ import taskRouter from "./routes/tasks.js";
 import { connectDB } from "./data/dataBase.js";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./utils/error.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin:[process.env.FRONT_END_URL],
+  methods:['PUT','POST','DELETE','GET'],
+  Credentials:true,
+}))
 app.use("/users", userRouter);
 app.use("/tasks", taskRouter);
 
